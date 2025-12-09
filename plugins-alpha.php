7ff9d7f01c46ce639cd3e0a0ebe0f5d1cc5c364d
wallacenana <walacenatanrodrigues@gmail.com>
2025-12-09 11:32:26 -0300

    atualização de versão para 2.0.4

--- plugins-alpha.php
+++ plugins-alpha.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Plugins Alpha
  * Description: Tudo o que você precisa para criar seus conteúdos na velocidade de 1 clique — Alpha Órion, Alpha Stories e muito mais.
- * Version: 2.0.2
+ * Version: 2.0.4
  * Author: Wallace Tavares
  * Author URI: https://pluginsalpha.com/
  * Text Domain: plugins-alpha
@@ -17,7 +17,7 @@
 
 if (!defined('ABSPATH')) exit;
 
-define('PLUGINS_ALPHA_VERSION', '2.0.2');
+define('PLUGINS_ALPHA_VERSION', '2.0.4');
 
 // Constantes
 define('PGA_FILE', __FILE__);
